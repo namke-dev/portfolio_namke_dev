e8ec4c83dc5b64b4c46191c7d905f8b65ecddef4
namke-dev <namke.dev@gmail.com>
2023-12-28 01:39:27 +0700

    Init avatar in intro component

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="z-[999] relative h-[5000px]">
+    <header className="z-[999]">
       <motion.div
         className="
         fixed top-0 left-1/2 
--- components/intro.tsx
+++ components/intro.tsx
@@ -1,19 +1,48 @@
+"use client";
 import Image from "next/image";
 import React from "react";
-
+import { motion } from "framer-motion";
 export default function Intro() {
   return (
     <section>
       <div className="flex items-center justify-center">
-        <div className="">
-          <Image
-            src={"https://avatars.githubusercontent.com/u/47099735"}
-            alt={"NamKe Portrait"}
-            width={192}
-            height={192}
-            quality={95}
-            priority={true}
-          />
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: "tween", duration: 0.3 }}
+          >
+            <Image
+              src={"/avatar.jpg"}
+              alt={"NamKe Portrait"}
+              width={192}
+              height={192}
+              quality={95}
+              priority={true}
+              className="h-24 w-24 rounded-full
+            border-[0.35rem] border-white
+            shadow-xl"
+            />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.15,
+              duration: 0.5,
+            }}
+          >
+            <span
+              className="
+              absolute
+              bottom-0 right-0
+              text-4xl"
+            >
+              👋
+            </span>
+          </motion.div>
         </div>
       </div>
     </section>
