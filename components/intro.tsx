0dd190049b97f2d23a14cdae62c3cc46b955ff8d
namke-dev <namke.dev@gmail.com>
2023-12-28 02:24:20 +0700

    add button in intro component

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,9 +2,16 @@
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 export default function Intro() {
   return (
-    <section>
+    <section
+      className="mb-28 max-w-[50rem]
+        text-center sm:mb-0"
+    >
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
@@ -45,6 +52,87 @@ export default function Intro() {
           </motion.div>
         </div>
       </div>
+
+      <motion.p
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
+        <span className="font-bold">full-stack developer</span> with{" "}
+        <span className="font-bold">8 years</span> of experience. I enjoy
+        building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">React (Next.js)</span>.
+      </motion.p>
+
+      <motion.div
+        className="flex flex-col sm:flex-row
+          items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href={"#contact"}
+          className="
+              group
+              flex items-center
+              bg-gray-900 text-white
+              gap-2 px-7 py-3
+              rounded-full
+              outline-none
+              focus:scale-110
+              hover:scale-110
+              hover:bg-gray-950
+              active:scale-105
+              cursor-pointer
+              transition"
+        >
+          Contact me here
+          <BsArrowRight className="group-hover:translate-x-2 opacity-70 transition" />
+        </Link>
+        <a
+          className="
+            group
+            flex items-center
+            bg-white 
+            gap-2 px-7 py-3
+            rounded-full
+            outline-none
+            focus:scale-110
+            hover:scale-110
+            active:scale-105
+            cursor-pointer
+            transition"
+          href="/CV-NguyenKeNam.pdf"
+          download={true}
+        >
+          Download CV
+          <HiDownload className="group-hover:translate-y-1 opacity-60 transition" />
+        </a>
+        <a
+          className="flex items-center
+          bg-white 
+            gap-2 p-4
+            rounded-full
+            text-gray-700
+            text-[1.35rem]"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          className="flex items-center
+          bg-white 
+            gap-2 p-4
+            rounded-full
+            text-gray-700
+            text-[1.35rem]"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }
