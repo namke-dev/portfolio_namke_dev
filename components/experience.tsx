1eaaecff08d15c7e26224506c525e1dbd990ebff
namke-dev <namke.dev@gmail.com>
2023-12-29 00:27:57 +0700

    implement Experience comonent layout and logic

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,16 +1,55 @@
 "use client";
+
 import React from "react";
 import { motion } from "framer-motion";
 import SectionHeading from "./section-heading";
 import { useSectionInView } from "@/lib/hooks";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { experiencesData } from "@/lib/data";
 
 export default function Experience() {
   const { ref } = useSectionInView("Experience");
 
   return (
-    <motion.section ref={ref} id="experience" className="scroll-mt-32">
+    <motion.section
+      ref={ref}
+      id="experience"
+      className="scroll-mt-28 mb-28 sm:mb-40"
+    >
       <SectionHeading>My Experience</SectionHeading>
-      <div className="h-[30rem]"></div>
+      <VerticalTimeline layout="2-columns" lineColor="">
+        {experiencesData.map((item, index) => (
+          <React.Fragment key={index}>
+            <VerticalTimelineElement
+              contentArrowStyle={{ borderRight: "0.4rem solid #9ca3af" }}
+              icon={item.icon}
+              iconStyle={{
+                background: "white",
+                fontSize: "1.5rem",
+              }}
+              visible={true}
+              contentStyle={{
+                background: "#f3f4f6",
+                boxShadow: "none",
+                border: "1px solid rgba(0, 0, 0, 0.05)",
+                textAlign: "left",
+                padding: "1.3rem 2rem",
+              }}
+              date={item.date}
+            >
+              <h3 className="font-semibold capitalize">{item.title}</h3>
+              <p className="font-normal !mt-0">{item.location}</p>
+              <p className="!font-normal !mt-1 text-gray-700">
+                {item.description}
+              </p>
+            </VerticalTimelineElement>
+          </React.Fragment>
+        ))}
+      </VerticalTimeline>
     </motion.section>
   );
 }
