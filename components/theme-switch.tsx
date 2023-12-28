191b0640f0958cf5d375d13e0dad914e838abe8b
namke-dev <namke.dev@gmail.com>
2023-12-29 03:33:48 +0700

    Let TailWind know when to switch theme

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -10,17 +10,25 @@ export default function ThemeSwitch() {
     if (theme === "light") {
       setTheme("dark");
       window.localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add("dark");
     } else if (theme === "dark") {
       setTheme("light");
       window.localStorage.setItem("theme", "light");
+      document.documentElement.classList.remove("dark");
     }
   };
   useEffect(() => {
     const localTheme = window.localStorage.getItem("theme") as Theme | null;
     if (localTheme) {
       setTheme(localTheme);
+
+      if (localTheme === "dark") {
+        document.documentElement.classList.add("dark");
+      }
     } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
       setTheme("dark");
+
+      document.documentElement.classList.add("dark");
     }
   }, [theme]);
 
