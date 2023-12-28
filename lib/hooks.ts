4bffe38a9aed5f5320a870f1db2f33628dd6d163
namke-dev <namke.dev@gmail.com>
2023-12-28 22:51:22 +0700

    addjust indicate condition in view

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -4,7 +4,7 @@ import { useInView } from "react-intersection-observer";
 import type { SectionName } from "./types";
 
 
-export function useSectionInView(sectionName: SectionName, threshold = 0.75){
+export function useSectionInView(sectionName: SectionName, threshold = 0.65){
   const { ref, inView } = useInView({
     threshold,
   });
