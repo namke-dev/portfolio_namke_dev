38a689303235f4867cd98de1e3a782d4a9d14d87
namke-dev <namke.dev@gmail.com>
2023-12-28 08:35:48 +0700

    Implement Project component logic

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,12 +1,22 @@
 "use client";
+
 import React from "react";
 import { motion } from "framer-motion";
 import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
+import Project from "./project";
 
 export default function Projects() {
   return (
     <motion.section>
       <SectionHeading>My Projects</SectionHeading>
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
     </motion.section>
   );
 }
